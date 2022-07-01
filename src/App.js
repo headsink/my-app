207ca2e7ba0a31ae911f08302ee9291b449e1240
headsink <headsink@gmail.com>
2022-07-01 19:16:22 +0800

    tailwind mainheader and main icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Mainheader from './component/mainheader';
 
 function App() {
   return (
-    <div className="App">
+    <div className='w-screen h-screen'>
+      <Mainheader></Mainheader>
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
---
+++ src/component/mainheader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import menuIcon from './assets/menu.svg'
+
+const Mainheader = () => {
+
+    return (
+        <div>
+            <img src={menuIcon} className="w-7" alt="Main Menu" />
+        </div>
+    );
+}
+
+export default Mainheader
