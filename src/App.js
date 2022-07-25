0992310919e3faf4980647b7749b16f34c07fc1a
headsink <headsink@gmail.com>
2022-07-25 22:03:39 +0800

    add form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { useState } from "react";
 
 function App() {
   const [num, setNum] = useState(0);
+  const items = [
+    {id : 1, name : 'berak'},
+    {id: 2, name: 'poop'}
+  ];
 
   const increaseNum = () => {
     setNum(num + 1);
@@ -12,8 +16,15 @@ function App() {
   return (
     <div className="w-screen h-screen">
       <Mainheader></Mainheader>
+      <form action="">
+        <input type="text" name="" id="" autoFocus />
+        <input type="submit" value="Save" />
+      </form>
       <h1>{num}</h1>
       <button onClick={increaseNum}>Increase</button>
+      <ul>
+        {items.map((item)=> <li>{item.id}-{item.name}</li>)}
+      </ul>
 
     </div>
   );
